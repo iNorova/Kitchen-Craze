8bd9d089f47eee9e31d010deeb919133cc467f6c
Marcus Gaspar <115352670+Marcus-Gaspar@users.noreply.github.com>
2024-11-20 22:58:57 +0800

    Minor changes
    Tinanggal ung pag pause pati music kasama

--- Kitchen Craze/Assets/Gameplay assets and scene/Script/Pausemenu.cs
+++ Kitchen Craze/Assets/Gameplay assets and scene/Script/Pausemenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.UIElements;
 public class Pausemenu : MonoBehaviour
 {
     public GameObject PausePanel;
-    public AudioSource BackgroundMusic;
+    
     void Update()
     {
 
@@ -14,12 +14,12 @@ public class Pausemenu : MonoBehaviour
 {
     PausePanel.SetActive(true);
     Time.timeScale = 0; 
-    BackgroundMusic.Pause();
+    
 }   
 public void Continue()
 {
     Time.timeScale = 1;
-    BackgroundMusic.UnPause();
+    
 }
 
 
